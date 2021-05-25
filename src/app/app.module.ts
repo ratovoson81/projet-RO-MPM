eaac2d3ebb2a663edf5bb2f607132da9673ac1a2
Anthony <Ratovoson81@gmail.com>
2021-05-25 09:15:12 +0300

    refact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { addTacheService } from './services/addTache.service';
-import { Routes, RouterModule } from '@angular/router';
-import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
-import { TacheService } from './models/TacheService';
-import { TacheListComponent } from "./tache-list/tache-List.component";
-import { GrapheComponent } from './graphe/graphe.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { addTacheService } from "./services/addTache.service";
+import { Routes, RouterModule } from "@angular/router";
+import { FourOhFourComponent } from "./four-oh-four/four-oh-four.component";
+import { TacheService } from "./models/TacheService";
+import { TacheListComponent } from "./tache-list/tache-list.component";
+import { GrapheComponent } from "./graphe/graphe.component";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
 
 const appRoutes: Routes = [
-  { path: '', component: TacheListComponent },
-  { path: 'taches', component: TacheListComponent },
-  { path: 'not-found', component: FourOhFourComponent },
-  { path: '**', redirectTo: 'not-found' }
+  { path: "", component: TacheListComponent },
+  { path: "taches", component: TacheListComponent },
+  { path: "not-found", component: FourOhFourComponent },
+  { path: "**", redirectTo: "not-found" },
 ];
 
 @NgModule({
@@ -32,14 +32,11 @@ const appRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(appRoutes),
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
   ],
   schemas: [NO_ERRORS_SCHEMA],
-  providers: [
-    addTacheService,
-    TacheService
-  ],
-  
-  bootstrap: [AppComponent]
+  providers: [addTacheService, TacheService],
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
